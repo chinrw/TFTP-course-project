4fd5e68e869b889b8e03b8468f3b04cfcd0aed7c
Rowen Qin <chinqrw@gmail.com>
2018-02-10 15:26:23 -0500

    little modified

--- main.cpp
+++ main.cpp
@@ -17,104 +17,106 @@
 #define ERROR 5
 #define PORT 2369
 
-void handle_read(struct sockaddr_in* sock_info, char* buffer, int buffer_len);
-void handle_write(struct sockaddr_in* sock_info, char* buffer, int buffer_len);
+void handle_read(struct sockaddr_in *sock_info, char *buffer, int buffer_len);
+
+void handle_write(struct sockaddr_in *sock_info, char *buffer, int buffer_len);
 
 
 int main() {
-	ssize_t n;
-	char buffer[BUF_LEN];
-	socklen_t sockaddr_len;
-	int server_socket;
-	struct sigaction act;
-	unsigned short int opcode;
-	unsigned short int * opcode_ptr;
-	struct sockaddr_in sock_info;
+    ssize_t n;
+    char buffer[BUF_LEN];
+    socklen_t sockaddr_len;
+    int server_socket;
+    struct sigaction act;
+    unsigned short int opcode;
+    unsigned short int *opcode_ptr;
+    struct sockaddr_in sock_info;
 
-	/* Set up interrupt handlers */
-	act.sa_handler = reinterpret_cast<void (*)(int)>(SIGCHLD);
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = 0;
-	sigaction(SIGCHLD, &act, NULL);
+    /* Set up interrupt handlers */
+    act.sa_handler = reinterpret_cast<void (*)(int)>(SIGCHLD);
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGCHLD, &act, NULL);
 
-	act.sa_handler = reinterpret_cast<void (*)(int)>(SIGALRM);
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = 0;
-	sigaction(SIGALRM, &act, NULL);
+    act.sa_handler = reinterpret_cast<void (*)(int)>(SIGALRM);
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGALRM, &act, NULL);
 
-	sockaddr_len = sizeof(sock_info);
+    sockaddr_len = sizeof(sock_info);
 
-	/* Set up UDP socket */
-	memset(&sock_info, 0, sockaddr_len);
+    /* Set up UDP socket */
+    memset(&sock_info, 0, sockaddr_len);
 
-	sock_info.sin_addr.s_addr = htonl(INADDR_ANY);
-	sock_info.sin_port = htons(PORT);
-	sock_info.sin_family = PF_INET;
+    sock_info.sin_addr.s_addr = htonl(INADDR_ANY);
+    sock_info.sin_port = htons(PORT);
+    sock_info.sin_family = PF_INET;
 
-	if ((server_socket = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("socket");
-		exit(-1);
-	}
+    server_socket = socket(PF_INET, SOCK_DGRAM, 0);
 
-	if (bind(server_socket, (struct sockaddr *)&sock_info, sockaddr_len) < 0) {
-		perror("bind");
-		exit(-1);
-	}
+    if (server_socket < 0) {
+        perror("socket");
+        exit(-1);
+    }
 
-	/* Get port and print it */
-	getsockname(server_socket, (struct sockaddr *)&sock_info, &sockaddr_len);
+    int bind_result = bind(server_socket, (struct sockaddr *) &sock_info, sockaddr_len);
+    if (bind_result < 0) {
+        perror("bind");
+        exit(-1);
+    }
 
-	printf("%d\n", ntohs(sock_info.sin_port));
+    /* Get port and print it */
+    getsockname(server_socket, (struct sockaddr *) &sock_info, &sockaddr_len);
 
-	/* Receive the first packet and deal w/ it accordingly */
-	while (1) {
-	intr_recv:
-		n = recvfrom(server_socket, buffer, BUF_LEN, 0,
-			(struct sockaddr *)&sock_info, &sockaddr_len);
-		if (n < 0) {
-			if (errno == EINTR) goto intr_recv;
-			perror("recvfrom");
-			exit(-1);
-		}
-		/* check the opcode */
-		opcode_ptr = (unsigned short int *)buffer;
-		opcode = ntohs(*opcode_ptr);
-		if (opcode != RRQ && opcode != WRQ) {
-			/* Illegal TFTP Operation */
-			*opcode_ptr = htons(ERROR);
-			*(opcode_ptr + 1) = htons(4);
-			*(buffer + 4) = 0;
-		intr_send:
-			n = sendto(server_socket, buffer, 5, 0,
-				(struct sockaddr *)&sock_info, sockaddr_len);
-			if (n < 0) {
-				if (errno == EINTR) goto intr_send;
-				perror("sendto");
-				exit(-1);
-			}
-		}
-		else {
-			if (fork() == 0) {
-				/* Child - handle the request */
-				close(server_socket);
-				break;
-			}
-			else {
-				/* Parent - continue to wait */
-			}
-		}
-	}
+    printf("%d\n", ntohs(sock_info.sin_port));
 
-	if (opcode == RRQ) handle_read(&sock_info, buffer, BUF_LEN);
-	if (opcode == WRQ) handle_write(&sock_info, buffer, BUF_LEN);
+    /* Receive the first packet and deal w/ it accordingly */
+    while (true) {
+        intr_recv:
+        n = recvfrom(server_socket, buffer, BUF_LEN, 0,
+                     (struct sockaddr *) &sock_info, &sockaddr_len);
+        if (n < 0) {
+            if (errno == EINTR) goto intr_recv;
+            perror("recvfrom");
+            exit(-1);
+        }
+        /* check the opcode */
+        opcode_ptr = (unsigned short int *) buffer;
+        opcode = ntohs(*opcode_ptr);
+        if (opcode != RRQ && opcode != WRQ) {
+            /* Illegal TFTP Operation */
+            *opcode_ptr = htons(ERROR);
+            *(opcode_ptr + 1) = htons(4);
+            *(buffer + 4) = 0;
+            intr_send:
+            n = sendto(server_socket, buffer, 5, 0,
+                       (struct sockaddr *) &sock_info, sockaddr_len);
+            if (n < 0) {
+                if (errno == EINTR) goto intr_send;
+                perror("sendto");
+                exit(-1);
+            }
+        } else {
+            if (fork() == 0) {
+                /* Child - handle the request */
+                close(server_socket);
+                break;
+            } else {
+                /* Parent - continue to wait */
+            }
+        }
+    }
 
-	return 0;
+    if (opcode == RRQ) handle_read(&sock_info, buffer, BUF_LEN);
+    if (opcode == WRQ) handle_write(&sock_info, buffer, BUF_LEN);
+
+    return 0;
 }
 
-void handle_read(struct sockaddr_in* sock_info, char* buffer, int buffer_len) {
+void handle_read(struct sockaddr_in *sock_info, char *buffer, int buffer_len) {
 
 }
 
-void handle_write(struct sockaddr_in* sock_info, char* buffer, int buffer_len) {
+void handle_write(struct sockaddr_in *sock_info, char *buffer, int buffer_len) {
 
 }
